a1791db249837c8fa741bf92e8fc4b629bb77526
Neha Sharma <nehasharma@Nehas-MacBook-Pro.local>
2019-10-25 19:45:51 +0530

    feat: Added forms example

---
+++ forms/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2>Shopping List!!</h2>
+    <FormCard />
+    <footer>made in VueJS</footer>
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+import FormCard from "./components/Form.vue";
+import style from './style/style.css';
+
+export default {
+  name: "app",
+   components: {
+    FormCard
+  }
+};
+</script>
+
